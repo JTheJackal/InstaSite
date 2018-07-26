8c988b6fc8b81344407300dddbeedbc37353e553
Joshua Styles <styles@outlook.com>
2018-07-26 15:03:23 +0100

    Database Tables Creation
    Successfully connects.
    Successfully creates tables.
    
    Syntax used for MariaDB. Will need to change for MySQL Server.

--- install/installAction.php
+++ install/installAction.php
@@ -31,8 +31,39 @@
         echo "Connected successfully";
     }
 
+    //Retrieve required SQL file.
+    $sqlSource = file_get_contents('../database/initOps.sql');
+    //$sqlSource = file('../database/initOps.sql');
+    //$sql_contents = SplitSQL($sqlSource);
+        
     //Create required tables in the Database.
-    
+    if (mysqli_query($connection, $sqlSource)) {
+    //if ($connection->query($sql_contents) === TRUE) {
+        echo "Tables created successfully";
+    } else {
+        echo "Error creating tables: " . $connection->error;
+    }
+
+    function SplitSQL($file, $delimiter = ';'){
+
+        $lines = file($file);
+        // Loop through each line
+        foreach ($lines as $line) {
+        // Skip it if it's a comment
+            if (substr($line, 0, 2) == '--' || $line == '')
+                continue;
+
+            // Add this line to the current segment
+            $templine .= $line;
+            // If it has a semicolon at the end, it's the end of the query
+            if (substr(trim($line), -1, 1) == ';') {
+                // Perform the query
+                mysql_query($templine);
+                // Reset temp variable to empty
+                $templine = '';
+            }
+        }
+    }
 ?>
 
     
