f531bf87f262059a77ec03e23051fac049eb5066
Joshua Styles <styles@outlook.com>
2018-07-27 11:26:36 +0100

    Structure and Includes
    Building the template and setting up includes.

--- index.php
+++ index.php
@@ -1,7 +1,70 @@
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="assets/css/stylesheet1.css">
         <title>InstaBuilder</title>
     </head>
 <body>
+
+    <div class="header">
+        <div class="row">
+            <div class="col-2"><h1>Header</h1></div>
+            <div class="col-5"></div>
+            <div class="col-5"><p>NavBar</p></div>
+        </div>
+    </div>
+    
+    <div class="container">    
+        <div class="row">
+            <div class="col-4 tile">
+                <p>IMG</p>
+                <div class="textBox">
+                    <?php include 'posts/testDescription.php';
+                        echo "<h3>$title1</h3><br><p>$description1</p>";
+                    
+                    ?>
+                </div>
+            </div>
+            <div class="col-4 tile">
+                <p>IMG</p>
+            </div>
+            <div class="col-4 tile">
+                <p>IMG</p>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-4 tile">
+                <p>IMG</p>
+            </div>
+            <div class="col-4 tile">
+                <p>IMG</p>
+            </div>
+            <div class="col-4 tile">
+                <p>IMG</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-4 tile">
+                <p>IMG</p>
+            </div>
+            <div class="col-4 tile">
+                <p>IMG</p>
+            </div>
+            <div class="col-4 tile">
+                <p>IMG</p>
+            </div>
+        </div>
+            
+        <div class="header">
+            <div class="row">
+                <div class="col-4"></div>
+                <div class="col-4"><p>NavBar</p></div>
+                <div class="col-4"></div>
+            </div>
+        </div>
+    </div>
+            
 </body>
 </html>
---
+++ posts/testDescription.php
@@ -0,0 +1,8 @@
+<?php
+
+    $title1 = "Lovely Title";
+    $description1 = "A lovely description.";
+
+    $title2 = "Lovely Title";
+    $description2 = "A lovely description.";
+?>
